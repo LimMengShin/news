968d33b8b00eb702d349353acc4e20dc2f2009d3
LimMengShin <62892957+LimMengShin@users.noreply.github.com>
2025-01-11 14:42:28 +0800

    Use newspaper instead of urltotext

--- update_server.py
+++ update_server.py
@@ -1,16 +1,12 @@
 import sqlite3
 from concurrent.futures import ThreadPoolExecutor
-from dotenv import load_dotenv
-import os
+import random
 
 from get_date import get_start_end_date
 from newsapi import call_news_api
 from urltotext import call_url_to_text_api
-from gemini.clean import clean_article
 
-load_dotenv()
-
-gemini_api_keys = os.getenv("GEMINI_API_KEY").split(",")
+THREADS = 2
 
 def split_articles(a, n):
     k, m = divmod(len(a), n)
@@ -18,9 +14,10 @@ def split_articles(a, n):
 
 start_date, end_date = get_start_end_date()
 articles_list = call_news_api(start_date=start_date, end_date=end_date)
-articles_list = list(split_articles(articles_list, len(gemini_api_keys)))
+random.shuffle(articles_list)
+articles_list = list(split_articles(articles_list, THREADS))
 
-def get_and_clean(articles, gemini_api_key):
+def insert_article(articles):
     articles_length = len(articles)
     for idx, article in enumerate(articles):
         print(f"ARTICLE {idx + 1}/{articles_length}")
@@ -29,13 +26,6 @@ def get_and_clean(articles, gemini_api_key):
         except Exception as e:
             print(f"Error fetching content for {article.get('url')}: {e}")
             continue
-
-        try:
-            article["content"] = clean_article(article["content"], gemini_api_key)
-        except Exception as e:
-            print(f"Error cleaning content for {article.get('url')}: {e}")
-            continue
-
 
         # insert to SQLite database
         connection = sqlite3.connect("news.db")
@@ -60,6 +50,6 @@ def get_and_clean(articles, gemini_api_key):
         connection.close()
 
 
-with ThreadPoolExecutor(max_workers=len(gemini_api_keys)) as executor:
+with ThreadPoolExecutor(max_workers=THREADS) as executor:
     for idx, articles in enumerate(articles_list):
-        executor.submit(get_and_clean, articles, gemini_api_keys[idx])
+        executor.submit(insert_article, articles)
